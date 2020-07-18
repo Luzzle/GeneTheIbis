dd4c7b97d0dac378d62fb722a3063e4102a47198
Luzzle <49113582+Luzzle@users.noreply.github.com>
2020-07-18 13:39:07 +1000

    Initial Commit

---
+++ options.js
@@ -0,0 +1,16 @@
+const options = {
+    options: {
+      debug: true
+    },
+    connection: {
+      reconnect: true
+    },
+    identity: {
+      username: "GeneTheIbis",
+      password: "oauth:kibxb2af5r6hnbzs6ynxbk2ctni05l"
+    },
+    channels: ['TheTrashCanArmy']
+  };
+  
+  
+  module.exports = options;
