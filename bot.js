d2806c31387a587174a16da0a9e64d9f0664aa61
Luzzle <cristianlustri@gmail.com>
2021-03-13 18:42:22 +1100

    Bot Init

--- bot.js
+++ bot.js
@@ -1,9 +1,39 @@
-// =========================================================================================
-//
-//     GENE THE IBIS© - A TWITCH BOT FOR THE TRASHCANARMY
-//     COPYRIGHT 2020 - 2021, CRISTIAN LUSTRI. ALL RIGHTS RESERVED
-//     BOT.JS
-//
-// =========================================================================================
+//! =========================================================================================
+//!
+//!     GENE THE IBIS© - A TWITCH BOT FOR THE TRASHCANARMY
+//!     COPYRIGHT 2020 - 2021, CRISTIAN LUSTRI. ALL RIGHTS RESERVED
+//!     BOT.JS
+//!
+//! =========================================================================================
 
-e
+//? NPM MODULES
+const tmi = require("tmi.js");
+const axios = require("axios");
+const env = require("dotenv").config({path: "keys.env"});
+
+//? CONSTANTS
+const VARS = process.env;
+
+//? BOT INIT
+const bot = new tmi.Client({
+    connection: {
+        secure: true,
+        reconnect: true
+    },
+    identity: {
+        username: "GeneTheIbis",
+        password: VARS.BOT_OAUTH_TOKEN
+    },
+    channels: ["TheTrashCanArmy"]
+});
+
+bot.connect();
+
+bot.once("connected", () => {
+    console.log("Connected to TheTrashCanArmy");
+})
+
+//? BOT COMMANDS
+bot.on("message", (channel, user, message, self) => {
+    console.log(message);
+})
